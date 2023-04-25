573be2aa6586288a8646a5f938dd21a909c2e532
Jack Maistros <jackmaistros@gmail.com>
2023-04-24 21:30:56 -0400

    Added util bar

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { Avatar, Grid } from '@nextui-org/react';
+import { Avatar, Popover, Button, Text,Modal, Grid } from '@nextui-org/react';
+import React from 'react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   //const linkedInIcon = boxicons.icons['linkedin'];
+  const [visible, setVisible] = React.useState(false);
+  const handler = () => setVisible(true);
+
+  const closeHandler = () => {
+    setVisible(false);
+    console.log("closed");
+  };
+
   return (
     <>
       <Head>
@@ -17,23 +27,99 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <Avatar text="Grad" 
-          color="error" 
-          textColor="white"
-          size= 'xl' 
-          zoomed/>
-     <a href="https://www.linkedin.com/in/jack-maistros/" target="_blank">
-        <Avatar text="Linkedin" 
-          color="primary" 
-          textColor="error"
-          size='lg' 
-          src="/IMAGES/linkedin-logo.png"
-          zoomed
-          squared
-          bordered
-        />
-      </a>
-      
+      <Grid.Container gap ={2} justify="center">
+        <Grid>
+        <Avatar text="profile-avatar" 
+            color="error" 
+            textColor="white"
+            size= 'xl' 
+            zoomed
+            src = "/IMAGES/profile-logo.jpeg"
+            pointer/>
+        </Grid>
+        <Grid>
+        <a href="https://www.linkedin.com/in/jack-maistros/" target="_blank">
+        <Avatar text="Linkedin-avatar" 
+            color="primary" 
+            textColor="error"
+            size='lg' 
+            src="/IMAGES/linkedin-logo.png"
+            zoomed
+            squared
+            bordered
+            pointer
+          />
+        </a>
+        </Grid>
+        <Grid>
+        <a href="https://github.com/jackmais" target="_blank">
+          <Avatar text="github-avatar" 
+            color="primary" 
+            textColor="error"
+            size='lg' 
+            src="/IMAGES/github-logo.png"
+            zoomed
+            squared
+            bordered
+            pointer
+          />
+        </a>
+        </Grid>
+        <Grid>
+        <Avatar text="pdf-avatar" 
+            color="primary" 
+            textColor="error"
+            size='lg' 
+            src="/IMAGES/pdf-logo.png"
+            zoomed
+            squared
+            bordered
+            pointer
+            onClick={handler}
+          />
+        </Grid>
+        <Grid>
+          <Avatar text="Mail-avatar" 
+            color="primary" 
+            textColor="error"
+            size='lg' 
+            src="/IMAGES/gmail-logo.png"
+            zoomed
+            squared
+            bordered
+            pointer
+            onClick={() => {
+              window.location.href = 'mailto:jackmaistros@gmail.com';
+            }}
+          />
+        </Grid>
+        <Modal
+          closeButton
+          aria-labelledby="modal-title"
+          open={visible}
+          onClose={closeHandler}
+          blur
+        >
+          <Modal.Header>
+            <Text id="modal-title" size={18}>
+                Would you like to download a PDF of my Resume?
+            </Text>
+          </Modal.Header>
+          <Modal.Body>
+          <Button onPress={closeHandler} style={{ marginBottom: '4px' }}>
+            Yes
+          </Button>
+          <Button flat color="error" onPress={closeHandler} style={{ marginTop: '4px', padding: '0' }}>
+            No
+          </Button>
+            
+          </Modal.Body>
+        </Modal>
+      </Grid.Container>
+
+      <Text style={{ color: 'white', fontSize: '48px', textAlign: 'center' }}>
+        Jack Maistros
+      </Text>
       </main>
     </>
   )
